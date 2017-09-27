0b398ea67892c782421f19dcf1f8543522aaa883
Elmansyah <elmansyah09@gmail.com>
2017-09-27 08:26:47 +0700

    Api PHP

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php 
+
+$data = array(
+	'data' => array("Elmansyah",
+		            "Fauzi",
+		            "Rachman",
+		            "Andri",
+		            "Riyadi",
+		            "Iqbalung",
+		            "Rofiq",
+		            "Bagas")
+);
+
+echo json_encode($data);
